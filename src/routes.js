740ee2741d72cc07a41701d19eca45a8b9f210a6
maximilianopaterno <129302888+maximilianopaterno@users.noreply.github.com>
2023-10-19 10:12:01 -0300

    Modificar rutas y agregar ruta para DeleteISBN
    Corregí las rutas de las funcionalidades.

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import { libro } from "./controller.js";
 export const router = Router()
 
 router.get('/libros', libro.getAll);
-router.post('/libros', libro.add);
-router.delete('/libros', libro.delete);
-router.put('/libros', libro.update);
+router.post('/libro', libro.add);
+router.delete('/libro', libro.delete);
+router.delete('/libro', libro.deleteISBN);
+router.put('/libro', libro.update);
