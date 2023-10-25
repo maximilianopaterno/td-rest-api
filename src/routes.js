3eab3154b19c5b96135f9eac2a51e061b178b4f4
maximilianopaterno <129302888+maximilianopaterno@users.noreply.github.com>
2023-10-25 10:48:52 -0300

    Se cargaron las rutas de add, delete y update routes.js

--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,11 @@ import { libro } from "./controller.js";
 
 export const router = Router()
 
+//--------->(Ruta para postman, metodo)
 router.get('/libros', libro.getAll);
 router.get('/libros/:id', libro.getOne);
+router.post('/libro', libro.add);
+router.delete('/libro/ISBN', libro.deleteISBN);
+router.delete('/libro/id', libro.deleteId);
+router.put('/libro', libro.updateISBN);
+router.put('/libro/id', libro.updateId);
